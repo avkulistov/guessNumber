13ee258461c96e7438645ca6beb32dd6d8086eae
Anton <akrevetka@gmail.com>
2019-06-09 14:35:46 +0700

    20190609 14.35

--- main.go
+++ main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"math/rand"
 	"net/http"
+	"strconv"
 )
+
+var randNumber = rand.Intn(10000)
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl, err := template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html")
@@ -17,10 +21,15 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func startHandler(w http.ResponseWriter, r *http.Request) {
-	numberForGuess := r.FormValue("numberForGuess")
-	userNumber := r.FormValue("userNumber")
-	fmt.Println(userNumber)
-	fmt.Println(numberForGuess)
+	if r.Method == "POST" {
+		numberForGuess := r.FormValue("numberForGuess")
+		userNumber := r.FormValue("userNumber")
+		fmt.Println("user num = " + userNumber)
+		fmt.Println("computer num = " + numberForGuess)
+		fmt.Println("random num = " + strconv.Itoa(randNumber))
+	} else {
+		randNumber = rand.Intn(10000)
+	}
 
 	tmpl, err := template.ParseFiles("templates/start.html", "templates/header.html", "templates/footer.html")
 	if err != nil {
@@ -28,14 +37,11 @@ func startHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	tmpl.ExecuteTemplate(w, "start", nil)
-
-	//r.FormValue("userNumber") = rand.Intn(10000)
 }
 
 func main() {
 	fmt.Println("listening on port :3000")
 
-	//http.Handle("/css/", http.FileServer(http.Dir("css/")))
 	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./css"))))
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/start", startHandler)
