89fcf093b8784563d5d9d1b82e35b5646966f5e3
Anton <akrevetka@gmail.com>
2019-06-10 12:42:57 +0700

    20190610 12.42

--- main.go
+++ main.go
@@ -6,13 +6,21 @@ import (
 	"math/rand"
 	"net/http"
 	"strconv"
+	"strings"
+	"time"
 )
 
 type viewData struct {
-	randNumber, userNumber, help string
+	RandNumber  string
+	UserNumber  string
+	Help        string
+	Attempts    int
+	TypeRandNum string
+	DisButton   string
 }
 
 var randNumber int
+var data viewData
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl, err := template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html")
@@ -24,22 +32,50 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func startHandler(w http.ResponseWriter, r *http.Request) {
-	var data viewData
-	if r.Method == "POST" {
-		userNumber := r.FormValue("userNumber")
-		fmt.Println("user num = " + userNumber)
-		fmt.Println("computer num = " + moreZeros(strconv.Itoa(randNumber), 4))
-		data.randNumber = moreZeros(strconv.Itoa(randNumber), 4)
-		data.userNumber = userNumber
-		data.help = ""
-	} else {
-		randNumber = rand.Intn(10000)
-		data.randNumber = moreZeros(strconv.Itoa(randNumber), 4)
-	}
-
+	rand.Seed(time.Now().Unix())
 	tmpl, err := template.ParseFiles("templates/start.html", "templates/header.html", "templates/footer.html")
 	if err != nil {
 		fmt.Fprintf(w, err.Error())
+	}
+
+	data.Help = ""
+	data.TypeRandNum = "password"
+	data.DisButton = ""
+
+	if r.Method == "POST" {
+		userNumber := r.FormValue("userNumber")
+		data.RandNumber = moreZeros(strconv.Itoa(randNumber), 4)
+		data.UserNumber = userNumber
+		data.Attempts++
+		if data.UserNumber == data.RandNumber {
+			data.RandNumber = moreZeros(strconv.Itoa(randNumber), 4)
+			data.TypeRandNum = "text"
+			data.DisButton = "disabled"
+			data.Help = "ПОБЕДА!"
+			tmpl.ExecuteTemplate(w, "start", data)
+			return
+		}
+		help := [4]string{"", "", "", ""}
+		//create help
+		userNumberS := strings.Split(data.UserNumber, "")
+		randNumberS := strings.Split(data.RandNumber, "")
+		for ind, value := range userNumberS {
+			if value == randNumberS[ind] {
+				help[ind] = "B"
+			} else if strings.Count(data.RandNumber, value) != 0 {
+				help[ind] = "K"
+			} else {
+				help[ind] = "  "
+			}
+		}
+		for _, value := range help {
+			data.Help += value
+		}
+	} else {
+		randNumber = rand.Intn(10000)
+		data.Attempts = 0
+		data.RandNumber = moreZeros(strconv.Itoa(randNumber), 4)
+		data.UserNumber = ""
 	}
 
 	tmpl.ExecuteTemplate(w, "start", data)
